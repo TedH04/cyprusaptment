cb57c93cd934fc579b8a53497f3b53d3edeeb2b5
TedH04 <tedhamren@gmail.com>
2024-01-18 14:52:07 +0100

    small css fix

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <header id="header" className="intro">
       <div className="overlay">
-        <Container textAlign="center">
+        <Container textAlign="center" className="textsContainer">
           <SemanticHeader as='h1' inverted>
             Hotel i Cypern Här
           </SemanticHeader>
