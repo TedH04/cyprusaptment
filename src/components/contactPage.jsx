9d51a15503a0db63e6de1ef8344a31677738eb3b
TedH04 <tedhamren@gmail.com>
2024-07-23 11:29:49 +0200

    small fix

--- src/components/contactPage.jsx
+++ src/components/contactPage.jsx
@@ -5,7 +5,7 @@ import './styling/contactPage.css';
 export const ContactPage = () => {
   return (
     <Container className="contact-page">
-      <SemanticHeader as='h2'>Contact Information</SemanticHeader>
+      <SemanticHeader as='h2'>Kontakt Information</SemanticHeader>
       <p>Name: Natasa Sillén</p>
       <p>Email: Natasasillen@gmail.com</p>
     </Container>
